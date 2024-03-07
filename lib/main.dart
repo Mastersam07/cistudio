da388bb63e36c9d52943b6956e0341d180029080
Mastersam07 <abadasamuelosp@gmail.com>
2024-03-07 21:03:06 +0100

    :tada: chore: create studio

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'src/workbench.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CI Steps Platform',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('CI Studio'),
+        ),
+        body: const Workbench(),
+      ),
+    );
+  }
+}
