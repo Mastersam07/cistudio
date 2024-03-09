c114f4213cbbb65f56d8e0763608a7563b2c8526
Mastersam07 <abadasamuelosp@gmail.com>
2024-03-09 07:42:55 +0100

    :white_check_mark: tests:custom property n export action

--- test/src/workbench_test.dart
+++ test/src/workbench_test.dart
@@ -59,33 +59,88 @@ void main() {
 
       expect(actualOrder, containsAllInOrder(expectedOrder));
     });
-  });
 
-  testWidgets(
-      'Selecting a property option from the dropdown updates the selection',
+    testWidgets(
+        'Selecting a property option from the dropdown updates the selection',
+        (WidgetTester tester) async {
+      // Build the app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
+
+      final reorderableListFinder = find.byType(ReorderableListView);
+      final stepFinder = find.descendant(
+        of: reorderableListFinder,
+        matching: find.byKey(const ValueKey('setup-&-cache-flutter')),
+      );
+      await tester.tap(stepFinder);
+      await tester.pumpAndSettle();
+
+      final cacheDropdownFinder = find.byKey(const ValueKey('dropdown-cache'));
+      await tester.ensureVisible(cacheDropdownFinder);
+      await tester.pumpAndSettle();
+      await tester.tap(cacheDropdownFinder);
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('without').last);
+      await tester.pumpAndSettle();
+
+      final DropdownButton<dynamic> dropdownButton =
+          tester.widget(cacheDropdownFinder);
+      expect(dropdownButton.value, equals('without'));
+    });
+
+    testWidgets(
+      'Entering a value in the flutterVersion text field updates the selection',
       (WidgetTester tester) async {
-    // Build the app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+        // Build the app and trigger a frame.
+        await tester.pumpWidget(const MyApp());
 
-    final reorderableListFinder = find.byType(ReorderableListView);
-    final stepFinder = find.descendant(
-      of: reorderableListFinder,
-      matching: find.byKey(const ValueKey('setup-&-cache-flutter')),
+        // Navigate to the "Setup & Cache Flutter" step settings.
+        final reorderableListFinder = find.byType(ReorderableListView);
+        final stepFinder = find.descendant(
+          of: reorderableListFinder,
+          matching: find.byKey(const ValueKey('setup-&-cache-flutter')),
+        );
+        await tester.tap(stepFinder);
+        await tester.pumpAndSettle();
+
+        // Find the flutterVersion text field.
+        final flutterVersionFieldFinder =
+            find.widgetWithText(TextField, 'Enter flutterVersion');
+
+        // Ensure the flutterVersion text field is visible.
+        await tester.ensureVisible(flutterVersionFieldFinder);
+        await tester.pumpAndSettle();
+
+        // Enter a new value in the flutterVersion text field.
+        await tester.enterText(flutterVersionFieldFinder, '3.16.0');
+        await tester.pump();
+
+        // Verify that the new value is updated in the widget.
+        final TextField textField = tester.widget(flutterVersionFieldFinder);
+        final TextEditingController? controller = textField.controller;
+        expect(controller?.text, equals('3.16.0'));
+      },
     );
-    await tester.tap(stepFinder);
-    await tester.pumpAndSettle();
 
-    final cacheDropdownFinder = find.byKey(const ValueKey('dropdown-cache'));
-    await tester.ensureVisible(cacheDropdownFinder);
-    await tester.pumpAndSettle();
-    await tester.tap(cacheDropdownFinder);
-    await tester.pumpAndSettle();
+    testWidgets('Tapping GitHub Actions button triggers export action',
+        (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
 
-    await tester.tap(find.text('without').last);
-    await tester.pumpAndSettle();
+      // Find the GitHub Actions button by text
+      final githubActionsButtonFinder =
+          find.widgetWithText(ElevatedButton, 'GitHub Actions');
+      expect(githubActionsButtonFinder, findsOneWidget);
 
-    final DropdownButton<dynamic> dropdownButton =
-        tester.widget(cacheDropdownFinder);
-    expect(dropdownButton.value, equals('without'));
+      // Tap the GitHub Actions button
+      await tester.tap(githubActionsButtonFinder);
+
+      await tester.pumpAndSettle();
+
+      // Check for a SnackBar in the ScaffoldMessenger
+      expect(find.byType(SnackBar), findsOneWidget);
+      expect(
+          find.text('CI workflow file has been downloaded.'), findsOneWidget);
+    });
   });
 }
